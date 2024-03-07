737d33b789f2600b0479ed3e85cd07b2cd736cde
Volatuz GP <87022695+Volatuz@users.noreply.github.com>
2024-03-06 19:51:13 -0500

    Add files via upload

---
+++ CodeFile1.cs
@@ -0,0 +1,41 @@
+﻿/*using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+class Program
+{
+    static void Main()
+    {
+        int nivel = 4; // Nivel de profundidad del diagrama de Hasse (puedes ajustarlo según tus necesidades)
+        int ancho = 800; // Ancho de la imagen en píxeles
+        int alto = 800; // Alto de la imagen en píxeles
+
+        using (Bitmap imagen = new Bitmap(ancho, alto))
+        using (Graphics graficos = Graphics.FromImage(imagen))
+        {
+            graficos.Clear(Color.White);
+
+            int espacioHorizontal = ancho / (nivel + 1);
+
+            DibujarNodos(graficos, espacioHorizontal, nivel, ancho / 2, 50, 0);
+
+            imagen.Save("DiagramaHasse.png", ImageFormat.Png);
+        }
+
+        Console.WriteLine("Diagrama de Hasse exportado como DiagramaHasse.png");
+    }
+
+    static void DibujarNodos(Graphics graficos, int espacio, int nivel, int x, int y, int profundidad)
+    {
+        if (profundidad > nivel)
+            return;
+
+        int radio = 20;
+
+        graficos.FillEllipse(Brushes.Blue, x - radio, y - radio, 2 * radio, 2 * radio);
+
+        DibujarNodos(graficos, espacio, nivel, x - espacio, y + 100, profundidad + 1);
+        DibujarNodos(graficos, espacio, nivel, x + espacio, y + 100, profundidad + 1);
+    }
+}
+*/
---
+++ graph.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using GiGraph.Dot.Entities.Graphs;
+using GiGraph.Dot.Entities.Nodes;
+using GiGraph.Dot.Types.Attributes;
+using GiGraph.Dot.Types.Colors;
+using GiGraph.Dot.Types.Graphs;
+using GiGraph.Dot.Types.Nodes;
+using GiGraph.Dot.Types.Orientation;
+using GiGraph.Dot.Types.Rankdir;
+
+namespace HasseDiagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var graph = new DotGraph();
+
+            graph.Attributes.Orientation = DotOrientation.Landscape;
+            graph.Attributes.RankDirection = DotRankDirection.LeftToRight;
+
+            int n = 5; // Cambia el valor de "n" según la cantidad de números binarios que desees representar.
+
+            for (int i = 0; i < (1 << n); i++)
+            {
+                var binaryString = Convert.ToString(i, 2).PadLeft(n, '0');
+
+                var node = new DotNode(binaryString);
+                node.Attributes.Shape = DotNodeShape.Circle;
+                node.Attributes.FontColor = DotColor.Transparent;
+
+                graph.Nodes.Add(node);
+
+                for (int j = 0; j < n; j++)
+                {
+                    if (binaryString[j] == '0')
+                    {
+                        var predNode = graph.Nodes.FirstOrDefault(n => n.Id == binaryString.Substring(0, j) + '1' + binaryString.Substring(j + 1));
+                        if (predNode != null)
+                        {
+                            graph.Edges.Add(node, predNode);
+                        }
+                    }
+                }
+            }
+
+            string dotContent = graph.Build();
+            File.WriteAllText("HasseDiagram.dot", dotContent);
+
+            // Ejecutar Graphviz para convertir el archivo DOT en una imagen PNG
+            var dotPath = "dot.exe"; // Cambia esto según la ubicación de dot.exe en tu sistema
+            var outputImagePath = "HasseDiagram.png";
+            var processStartInfo = new ProcessStartInfo(dotPath, $"-Tpng HasseDiagram.dot -o {outputImagePath}");
+            Process.Start(processStartInfo);
+
+            Console.WriteLine("Diagrama de Hasse creado como HasseDiagram.png");
+        }
+    }
+}
